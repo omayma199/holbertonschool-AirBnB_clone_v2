b32c2e76a3b5a7367fdffca42d4d255ae6d108f5
oumayma <omayma199@github.com>
2023-04-14 00:59:50 +0100

    sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Create database and user for testing
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+FLUSH PRIVILEGES;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
