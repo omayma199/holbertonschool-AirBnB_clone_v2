be87ea4c71b6ce6337c980afd24038834b1c9a16
oumayma <omayma199@github.com>
2023-04-14 16:43:12 +0100

    flask

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,31 +1,34 @@
-
-#!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
-    """Render template with states
-    """
-    path = '9-states.html'
-    states = storage.all(State)
-    return render_template(path, states=states, id=id)
+storage.all()
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
+def teardown_data(self):
+    """
+        refresh data
     """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+@app.route('/states', strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id=None):
+    """
+        list state by id if found
+    """
+    info = []
+    states = storage.all(State)
+    if id is None:
+        for k in states:
+            info.append(states[k])
+    else:
+        id = 'State.' + id
+        info = states.get(id)
+    return render_template('9-states.html', states=info, id=id)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
